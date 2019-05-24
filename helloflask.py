8e4d4172cfbeeb387d6d6ac231d2b21957105a90
Shivam <shivam@abc.com>
2019-05-24 19:46:25 +0530

    multiple url routing 1

--- helloflask.py
+++ helloflask.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def hello():
     return "<h1>Hello Flask!</h1>"
+
+@app.route("/about")
+def about():
+    return "About Page"
 
 if __name__=="__main__":
     app.run(debug = True)
