c59515938389a3c6735d7e2f3daaf0aed6b33927
Shivam <shivam@abc.com>
2019-05-25 13:01:26 +0530

    variables passed to templates

--- helloflask.py
+++ helloflask.py
@@ -1,14 +1,32 @@
 from flask import Flask , render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'Shivam',
+        'title':'blog post 1',
+        'content':'blog post 1 content',
+        'date_posted':'25 May 2019'
+    
+    },
+    {
+        'author':'Anna',
+        'title':'blog post 2',
+        'content':'blog post 2 content',
+        'date_posted':'20 May 2019'
+    
+    }
+]
+
+
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html",posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",title = 'About Page')
 
 if __name__=="__main__":
     app.run(debug = True)
